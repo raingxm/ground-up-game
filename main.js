a1d677e04324977ef032db17d89d752f89233d6e
raingxm <raingxm@163.com>
2015-07-07 22:08:19 +0800

    draw sky different color according to current time

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
         var request = new XMLHttpRequest();
         request.onload = function(data) {
             var ctx = document.getElementById('mysky').getContext('2d');
-            var time = JSON.parse(data.target.responseText);
-            console.log(time);            
+            var time = JSON.parse(data.target.responseText).time;
+            var skyColor = time === "day" ? 'blue' : 'black';
+            ctx.fillStyle = skyColor;
+            ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         }
         request.open("GET", "/time.json", true);
         request.send();
